c21e2b52dfb64020dd8f22a0fb30c303c853bea1
rpsamples <redpandadocs@gmail.com>
2022-04-13 09:05:47 +0000

    Added .env file fixed schema

--- redpanda_producer.py
+++ redpanda_producer.py
@@ -1,11 +1,14 @@
+import json
+import os
 from kafka import KafkaProducer, KafkaAdminClient
 from kafka.admin import NewTopic
 from time import sleep
 from faker import Faker
-import json
+from dotenv import load_dotenv
 
 
 def main():
+    load_dotenv()
     topic_name1 = "fitbit_activity_tracker"
     topic_name2 = 'fitbit_owner'
     modes_of_excercise = ['cycling', 'skiing', 'walking', 'jogging', 'running', 'swimming', 'climbing',
@@ -13,24 +16,25 @@ def main():
     fake = Faker()
     owner_id = 0
     activity_id = 0
+    broker_ip = os.environ.get('REDPANDA_BROKER_IP')
 
     try:
         # Create Kafka topic
         topic1 = NewTopic(name=topic_name1, num_partitions=1, replication_factor=1)
         topic2 = NewTopic(name=topic_name2, num_partitions=1, replication_factor=1)
-        admin = KafkaAdminClient(bootstrap_servers="localhost:9092")
+        admin = KafkaAdminClient(bootstrap_servers=broker_ip)
         admin.create_topics([topic1, topic2])
     except Exception:
         print(f"Topic {topic_name1, topic_name2} is already created")
 
-    producer = KafkaProducer(bootstrap_servers=["localhost:9092"],
+    producer = KafkaProducer(bootstrap_servers=[broker_ip],
                              value_serializer=lambda m: json.dumps(m).encode('ascii'))
     for i in range(100000):
         fake_date = fake.date_this_year(True, True)
         name = fake.name()
         owner_id += 1
         str_date = str(fake_date)
-        producer.send(topic_name2, {'fitbit_owner': name, 'owner_id': owner_id, 'date': str_date})
+        producer.send(topic_name2, {'owner_name': name, 'owner_id': owner_id, 'date': str_date})
         num_activities = fake.pyint(1,5)
         single_activity = 1
         while single_activity <= num_activities:
@@ -44,9 +48,9 @@ def main():
                                        'calories_burnt': calories, 'active_mins': active_minutes,
                                        'sedentary_mins': sedentary_minutes, 'owner_id': owner_id, 'activity_id': activity_id})
             single_activity += 1
-        print(f"Inserted entry '{name}' to '{topic_name2}'")
-        print(f"Inserted {num_activities} activities against '{name}' in '{topic_name1}'")
-        sleep(10)
+        print(f"Inserted owner '{name}' to '{topic_name2}'")
+        print(f"Inserted {num_activities} activities against '{name}' in '{topic_name1}'\n")
+        sleep(0.5)
 
 
 main()
